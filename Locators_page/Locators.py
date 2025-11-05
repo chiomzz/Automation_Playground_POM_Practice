ed2f27e5a9411474911d3ee71a5611f78f801420
chiomzz <chiomaikerionwu@gmail.com>
2025-11-04 21:38:03 -0600

    update of locators page

--- Locators_page/Locators.py
+++ Locators_page/Locators.py
@@ -10,16 +10,20 @@ class LoginLocators:
 class NewCustomerLocators:
     NEW_CUSTOMER = (By.ID, 'new-customer')
 
+
 class AddCustomerLocators:
     CUSTOMER_EMAIL = (By.CSS_SELECTOR, '#EmailAddress')
     FIRSTNAME = (By.CSS_SELECTOR, '#FirstName')
     LASTNAME = (By.CSS_SELECTOR, '#LastName')
     CITY = (By.CSS_SELECTOR, '#City')
     STATE = (By.CSS_SELECTOR, '#StateOrRegion')
-    SELECT_STATE = (By.CSS_SELECTOR, '')
+    SELECT_STATE = (By.CSS_SELECTOR, '#StateOrRegion > option:nth-child(46)')
     GENDER = (By.XPATH, '//*[@id="loginform"]/div/div/div/div/form/div[6]/input[2]')
     PROMOTIONAL_LIST = (By.XPATH, '//*[@id="loginform"]/div/div/div/div/form/div[7]/input')
     SUBMIT_CUSTOMER = (By.XPATH, '//*[@id="loginform"]/div/div/div/div/form/button')
+
+class Logout:
+    SIGN_OUT = (By.CSS_SELECTOR, 'body > nav > ul > li > a')
 
 
 
