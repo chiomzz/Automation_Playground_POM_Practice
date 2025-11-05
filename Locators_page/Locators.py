2261e032ac6147225c736f8e338ad0bcb7f5c99d
chiomzz <chiomaikerionwu@gmail.com>
2025-11-04 21:11:12 -0600

    Locators Page

---
+++ Locators_page/Locators.py
@@ -0,0 +1,25 @@
+from selenium.webdriver.common.by import By
+
+
+class LoginLocators:
+    EMAIL_ADDRESS = (By.ID, 'email-id')
+    PASSWORD = (By.CSS_SELECTOR, '#password')
+    SUBMIT = (By.XPATH, '//*[@id="submit-id"]')
+
+
+class NewCustomerLocators:
+    NEW_CUSTOMER = (By.ID, 'new-customer')
+
+class AddCustomerLocators:
+    CUSTOMER_EMAIL = (By.CSS_SELECTOR, '#EmailAddress')
+    FIRSTNAME = (By.CSS_SELECTOR, '#FirstName')
+    LASTNAME = (By.CSS_SELECTOR, '#LastName')
+    CITY = (By.CSS_SELECTOR, '#City')
+    STATE = (By.CSS_SELECTOR, '#StateOrRegion')
+    SELECT_STATE = (By.CSS_SELECTOR, '')
+    GENDER = (By.XPATH, '//*[@id="loginform"]/div/div/div/div/form/div[6]/input[2]')
+    PROMOTIONAL_LIST = (By.XPATH, '//*[@id="loginform"]/div/div/div/div/form/div[7]/input')
+    SUBMIT_CUSTOMER = (By.XPATH, '//*[@id="loginform"]/div/div/div/div/form/button')
+
+
+
